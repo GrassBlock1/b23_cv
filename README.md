2a4b7447c7a6d8279c04d7c10bd60e5e79ef619f
grassblock <hi@imgb.space>
2024-08-02 18:07:03 +0800

    chore: update license

--- README.md
+++ README.md
@@ -52,4 +52,21 @@ python bcv.py https://www.bilibili.com/read/cv12345678
 目前这个脚本只是一个“能用”的下载器，如果有更多的需求或者想要添加更多的功能，欢迎提交 Issue 和 Pull Request。
 
 ## 许可证
-GNU General Public License v3.0
+GNU General Public License v3.0:
+
+    b23_cv - simple bilibili column downloader
+    Copyright (C) 2024  Grassblock
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
--- bcv.py
+++ bcv.py
@@ -1,9 +1,37 @@
+"""
+    b23_cv - simple bilibili column downloader
+    Copyright (C) 2024  Grassblock
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+"""
+
 import argparse
 
 from b23_cv import get_single, get_batch
 
 parser = argparse.ArgumentParser(
-    description='Download articles from Bilibili, refer to the README for more information.')
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    description="""
+    Download articles from Bilibili, refer to the README for more information.\n\n
+        b23_cv  Copyright (C) 2024  GrassBlock 
+    This program comes with ABSOLUTELY NO WARRANTY; 
+    This is free software, and you are welcome to redistribute it 
+    under certain conditions; 
+    refer to the LICENSE file for more information.
+    """)
 parser.add_argument('URL', metavar='URL', nargs=1, help='the URL of the '
                                                         'article/readlist or '
                                                         'the column')
